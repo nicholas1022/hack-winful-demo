8bf634c4182182d0257317e426b0e595b2bdd690
Nicholas Lee <nicholas_1022@hotmail.com>
2024-03-23 20:06:44 -0400

    backend init

---
+++ ord-sys-bn/src/main/java/com/winful/ordsys/OrdsysApplication.java
@@ -0,0 +1,13 @@
+package com.winful.ordsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdsysApplication.class, args);
+	}
+
+}
---
+++ ord-sys-bn/src/test/java/com/winful/ordsys/OrdsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winful.ordsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
