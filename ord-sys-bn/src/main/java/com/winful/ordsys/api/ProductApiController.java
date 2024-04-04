0d184004f28f523ecb6e1dbcd895170f8028ce1a
Nicholas Lee <nicholas_1022@hotmail.com>
2024-04-03 23:05:24 -0400

    Enable CORS

--- ord-sys-bn/src/main/java/com/winful/ordsys/api/ProductApiController.java
+++ ord-sys-bn/src/main/java/com/winful/ordsys/api/ProductApiController.java
@@ -3,6 +3,7 @@ package com.winful.ordsys.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Generated;
@@ -10,6 +11,7 @@ import java.util.Optional;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-03-31T19:52:06.090125-04:00[America/Toronto]", comments = "Generator version: 7.4.0")
 @Controller
+@CrossOrigin
 @RequestMapping("${openapi.hackathonWinful.base-path:/api}")
 public class ProductApiController implements ProductApi {
 
