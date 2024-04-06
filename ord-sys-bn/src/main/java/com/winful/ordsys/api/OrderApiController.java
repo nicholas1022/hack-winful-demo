1953e3681f9a5ce2fad7070cdc4c1d9a8f9b458a
Nicholas Lee <nicholas_1022@hotmail.com>
2024-04-06 02:11:07 -0400

    enable cors of order controller

--- ord-sys-bn/src/main/java/com/winful/ordsys/api/OrderApiController.java
+++ ord-sys-bn/src/main/java/com/winful/ordsys/api/OrderApiController.java
@@ -3,6 +3,7 @@ package com.winful.ordsys.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Generated;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-04-06T00:29:16.910561-04:00[America/Toronto]", comments = "Generator version: 7.4.0")
 @Controller
 @RequestMapping("${openapi.hackathonWinful.base-path:/api}")
+@CrossOrigin
 public class OrderApiController implements OrderApi {
 
     private final OrderApiDelegate delegate;
